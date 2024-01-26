aeab81df75f2cadc8e035b361df61fc9fa9fcf7b
JesusBautistaPartida <jbaupar475@iescarrillo.es>
2024-01-26 08:52:59 +0100

    maniManitas

---
+++ src/main/java/com/example/maniManitas/ManiManitasApplication.java
@@ -0,0 +1,13 @@
+package com.example.maniManitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManiManitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManiManitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maniManitas/ManiManitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maniManitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManiManitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
